546601ea82dc736f157d73a4f1a619f3741c5229
권태용 <@192.168.0.2>
2022-03-27 00:55:59 +0900

    launch the git

---
+++ java project/src/ca.java
@@ -0,0 +1,14 @@
+public class ca{
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
+	
+	public static final int sum(int n,int m) {
+		
+	}
+}
+
+227	
+2	
+2.5	
