dec6c7ff6889e2159b2363f27134071634e06f21
권태용 <@192.168.0.2>
2022-03-27 01:01:58 +0900

    git

---
+++ java project/src/ca.java
@@ -0,0 +1,17 @@
+public class ca{
+	public static void main(String[] args) {
+		int i,sum=0;
+		
+		for(i=1; i<=10; i++) {
+			sum+=i;
+			System.out.print(i);
+			
+			if(i<=9)
+				System.out.print("+");
+			else {
+				System.out.print("=");
+				System.out.print(sum);
+			}
+		}
+	}
+}
