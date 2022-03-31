cb5f2468b86752ef19baedd7e9d9a70990b014e0
권태용 <@192.168.0.2>
2022-03-31 15:00:36 +0900

    d

---
+++ Return_Array/src/return_array.java
@@ -0,0 +1,19 @@
+
+public class return_array {
+	
+	static int[] makeArray() {
+		int temp[]=new int[4];
+		for(int i=0; i<temp.length; i++)
+			temp[i]=i;
+			return temp;
+	}
+	
+	public static void main(String[] args) {
+		int intArray[];
+		intArray=makeArray();
+		
+		for(int i=0; i<intArray.length; i++) 
+			System.out.print(intArray[i]+" ");
+		
+	}
+}
---
+++ create/src/create/Hello.java
@@ -0,0 +1,17 @@
+package create;
+import java.util.Scanner;
+public class Hello {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int dividend;
+		int divisor;
+		
+		System.out.println("나뉨수를 입력하시오");
+		dividend=scanner.nextInt();
+		System.out.println("나눗수를 입력하시오");
+		divisor=scanner.nextInt();
+		System.out.println(dividend+"를"+divisor+"로 나누면 몫은"+dividend/divisor+"입니다");
+
+		
+	}
+}
